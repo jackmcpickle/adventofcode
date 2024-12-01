2f28527f4e2314ee98ed591c60e2acd209109023
Jack McNicol <jackmcpickle@gmail.com>
2024-12-01 23:28:50 +1030

    day 1

---
+++ 2024/day1/index.ts
@@ -0,0 +1,18 @@
+import { input } from './input';
+
+const locationOne = input.flatMap((row) => row[0]).sort();
+const locationTwo = input.flatMap((row) => row[1]).sort();
+
+const totalDistance = locationOne.reduce((acc, curr, index) => {
+    acc += Math.abs(curr - locationTwo[index]);
+    return acc;
+}, 0);
+
+console.log(totalDistance);
+
+const similarity = locationOne.reduce((acc, curr) => {
+    acc += curr * locationTwo.filter((item) => item === curr)?.length;
+    return acc;
+}, 0);
+
+console.log(similarity);
