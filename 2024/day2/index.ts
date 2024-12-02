4e26540524506dc7660739e735a42e918b4b2611
Jack McNicol <jackmcpickle@gmail.com>
2024-12-02 20:20:23 +1030

    day 2 part 2

--- 2024/day2/index.ts
+++ 2024/day2/index.ts
@@ -11,7 +11,17 @@ const safeRows = reports.reduce((acc, report) => {
     ) {
         return acc + 1;
     }
+    for (let i = 0; i < report.length; i++) {
+        const reducedReport = [...report];
+        reducedReport.splice(i, 1);
+        if (
+            isDifferenceInLevelsBetweenOneAndThree(reducedReport) &&
+            allSingleDirection(reducedReport)
+        ) {
+            return acc + 1;
+        }
+    }
     return acc;
 }, 0);
 
-console.log(safeRows);
+console.log({ safeRows });
